7bf0a6cdd0d67f6321218ddcc666e55d956409cf
alnickelzon <al.villanueva1234@gmail.com>
2022-10-14 17:55:35 +0800

    navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { logo, iconarrowdown, iconarrowup } from "../images";
+import { useState } from "react";
+import {
+  logo,
+  iconarrowdown,
+  iconarrowup,
+  iconmenu,
+  iconclose,
+} from "../images";
 import { menu, buttons } from "../Constants";
 
 const Navbar = () => {
+  const [togglemenu, setToggle] = useState(false);
+
   return (
-    <div className="flex p-[1.5rem] flex-auto justify-between items-end">
+    <div className="flex p-[1.5rem] flex-auto justify-between items-center bg-slate-100">
       <img src={logo} alt="logo" className="max-h-[15px]" />
 
       {/* desktop */}
@@ -12,7 +21,7 @@ const Navbar = () => {
         <div className="flex menu">
           {menu.map((menu, index) => (
             <div
-              className={`flex items-center ${
+              className={`flex items-center cursor-pointer hover:text-indigo-600 ${
                 index === menu.length - 1 ? "mr-0" : "mr-4"
               }`}
             >
@@ -25,7 +34,9 @@ const Navbar = () => {
         <div className="buttons">
           {buttons.map((btn, index) => (
             <button
-              className={`${index === buttons.length - 1 ? "mr-0" : "mr-5"}`}
+              className={`${
+                index === buttons.length - 1 ? "mr-0" : "mr-5"
+              } border-[1px] border-black rounded-xl p-1 hover:bg-slate-300`}
             >
               {btn.title}
             </button>
@@ -34,6 +45,43 @@ const Navbar = () => {
       </div>
 
       {/* mobile */}
+      <div className=" ml-3 flex ss:hidden text-sm">
+        <img
+          src={!togglemenu ? iconmenu : iconclose}
+          alt="mobile-menu"
+          className="max-h-[19px] z-50"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            !togglemenu ? "hidden" : "flex"
+          } flex-col absolute top-0 right-0 min-w-[40%] items-start bg-slate-50 min-h-full p-6 py-[5rem] sidebar`}
+        >
+          {menu.map((menu, index) => (
+            <div
+              className={`flex items-center cursor-pointer hover:text-indigo-600 ${
+                index === menu.length - 1 ? "mb-0" : "mb-4"
+              }`}
+            >
+              {menu.name}
+              <img src={menu.icon} className="ml-1" />
+            </div>
+          ))}
+
+          <div className="buttons flex flex-col items-center mt-10">
+            {buttons.map((btn, index) => (
+              <button
+                className={`${
+                  index === buttons.length - 1 ? "mb-0" : "mb-5"
+                } border-[1px] border-black rounded-xl p-1 hover:bg-slate-300 min-w-[10rem]`}
+              >
+                {btn.title}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
